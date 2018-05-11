c7ed39f7d3f6b9f4e7ec5d65ec0b5e9f787fc16b
micky387 <mickaelsaibi@free.fr>
2018-05-11 14:09:08 +0200

    Add support for more device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,7 +99,7 @@ TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
 # MultiROM config. MultiROM also uses parts of TWRP config
 TARGET_RECOVERY_IS_MULTIROM := true
-MR_DEVICE_VARIANTS := tblte tbltexx tbltedt
+MR_DEVICE_VARIANTS := tblteatt tbltecan tblte tbltexx tbltedt tbltetmo tblteusc tbltespr tbltevzw
 MR_NO_KEXEC := enabled
 MR_NO_KEXEC := true
 MR_INPUT_TYPE := type_b
